5d7bcfb3b76b1a2db914b00f0247f0c449da4471
leen-duafi <leen-hala@hotmail.com>
2021-08-11 21:08:17 +0300

    ok

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 const JWKSURI = process.env.JWKSURI;
 const mongoose = require("mongoose");
-const getbooks=require('./controllers/book');
+const { getbooks, addBook, deletBook } = require('./controllers/book');
 const app = express();
 
 // const MONGO_DB_URL = process.env.MONGO_DB_URL;
 mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -31,45 +32,43 @@ app.get('/', (request, response) => {
   response.send('Hello World 🥳');
 });
 
-// function getKey(header, callback) {
-//   client.getSigningKey(header.kid, function (err, key) {
-//     var signingKey = key.publicKey || key.rsaPublicKey;
-//     callback(null, signingKey);
-//   });
-// }
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function (err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
 
 
-// app.get('/test', (request, response) => {
+app.get('/test', (request, response) => {
 
-//   // TODO: 
-//   // STEP 1: get the jwt from the headers
-//   const token = request.headers.authorization.split(' ')[1];
-//   console.log(token)
-//   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+  // TODO: 
+  // STEP 1: get the jwt from the headers
+  const token = request.headers.authorization.split(' ')[1];
+  console.log(token)
+  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
 
-//   jwt.verify(token, getKey, {}, (error, user) => {
-//     if (error) {
-//       response.send('invalid token');
-//     }
-//     response.json(user);
-//   })
+  jwt.verify(token, getKey, {}, (error, user) => {
+    if (error) {
+      response.send('invalid token');
+    }
+    response.json(user);
+  })
 
-//   response.send("got your token ⭐");
-//   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken  ////DONE////
-//   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+  response.send("got your token ⭐");
+  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken  ////DONE////
+  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
-// })
+})
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
-//...................................................................//
-
-
-
+//...............................lab 13....................................//
+app.post('/new', addBook);
+//...............................lab 14....................................//
+app.post('/delet/:book_id', deletBook);
 //...............................lab12.............................//
-
 // app.get('/books', (request, response) => {
 //   response.send('Lab 12 was done😌🎉🎊');
 // });
-
-app.get('/books',getbooks);
+app.get('/books', getbooks);
